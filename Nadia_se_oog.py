c32fbc2df85b53a0758a1d8ec3ae59a78aaab68f
wpswart <wpswart006@gmail.com>
2018-06-08 15:53:42 +0200

    End of day on a June 8th

--- Nadia_se_oog.py
+++ Nadia_se_oog.py
@@ -1,8 +1,11 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.special import comb
 
-rgb = cv2.imread('../IMG_7187.JPG')
+
+#rgb = cv2.imread('../IMG_7192.JPG')
+rgb = cv2.imread('2018-06-08-100355_201x109_scrot.png')
 rgb = cv2.resize(rgb,(640,480))
 
 r = rgb[:,:,2]
@@ -22,5 +25,24 @@ rp = 128 * (1 + (1-np.power(_b,r/(1.5*mu))/(a*np.power(_b,r/(1.5*mu))+1)))
 gp = 128 * (1 + (1-np.power(_b,g/(1.5*mu))/(a*np.power(_b,g/(1.5*mu))+1)))
 bp = 128 * (1 + (1-np.power(_b,b/(1.5*mu))/(a*np.power(_b,b/(1.5*mu))+1)))
 
-I = bp+gp-rp
-plt.imshow(I)
+I = (bp+gp-rp).astype('uint8')
+I = cv2.equalizeHist(I).astype('int16')
+#hist = cv2.calcHist([I],[0],None,[256],[0,256])
+#plt.hist(I.ravel(),256,[0,256]); plt.show()
+#plt.imshow(I, cmap = 'gray')
+
+gray_levels = np.unique(I)
+gray_levels_p =np.zeros(comb(gray_levels.size,2).astype('int16'))
+
+#qi = gl graay_levels[j] = qj
+for k,gl in enumerate(gray_levels):
+    for j in range(k+1,gray_levels.size):
+        sum_1 = np.log(np.abs(gl - gray_levels[j])+1)
+        sum_2 = np.abs(np.where(I ==gl)[0].size*gl-np.where(I ==gray_levels[j])[0].size*gray_levels[j])/I.size
+#        sum_3 = 
+        gray_levels_p[k] = sum_1+sum_2
+      
+        
+    
+
+
