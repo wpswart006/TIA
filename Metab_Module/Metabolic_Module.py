e6f6415881193884542087bca98a46322ab60cd7
DESKTOP-TS903NN\WP <wpswart006@gmail.com>
2018-08-20 11:46:19 +0200

    Added a readme, some docstring and a requiremntes file

--- Metab_Module/Metabolic_Module.py
+++ Metab_Module/Metabolic_Module.py
@@ -1,22 +1,32 @@
 from keras.models import load_model
 import numpy as np
+
+
 class Metabolic:
-    
-   def __init__(self,scale = 210,mean = 0.1806061555437761,path_to_model = 'metabolic_model.h5'):
+
+   def __init__(self, scale=210, mean=0.1806061555437761, path_to_model='metabolic_model.h5'):
+       """Inititalize the Metabolic module with default paramaters for normalizing the data (scale being the largest value in the dataset, and mean
+       being the arithmitic mean of the value's present), and a path to the tensorflow mdel
+       """
        self.model = load_model(path_to_model)
-       self.scale =scale
+       self.scale = scale
        self.mean = mean
-       
-   def predict(self,age,waist,systolic):
-       x = np.array([[age,waist,systolic]]).astype('float32')
+
+   def predict(self, age, waist, systolic):
+       """ Prediction function that takes a patient's age, waist circumference in centimeter and systolic blood presure in mm/hg
+       and returns the predicted probability as a percentage of certainty in the form [chance being not at risk,chance being at risk]
+       """
+       x = np.array([[age, waist, systolic]]).astype('float32')
        x /= self.scale
        x -= self.mean
        return self.model.predict(x)
-   
-   def predict_to_class(self,age,waist,systolic):
-       res =self.predict(age,waist,systolic)
+
+   def predict_to_class(self, age, waist, systolic):
+       """ Prediction function that takes a patient's age, waist circumference in centimeter and systolic blood presure in mm/hg
+       and calls the predict function to retrun a string that indicates the class
+       """
+       res = self.predict(age, waist, systolic)
        if np.argmax(res) == 0:
            return "Little Metabolic Syndrome Risk"
        elif np.argmax(res) == 1:
            return "At risk of Metabolic Syndrome"
-       
